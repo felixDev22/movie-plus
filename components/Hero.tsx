8983baac528f6cf94f8735f442000a867d629057
felixDev22 <felixnzomo@gmail.com>
2023-08-05 00:35:17 +0300

    Style the header and the overview

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,15 +17,22 @@ function Hero({ netflixOriginals }: Props ){
 
 
   return (
-    <div>
+    <div className="flex flex-col space-y-3 lg:pb-12 lg:h-[70vh] lg:justify-end sm:h-[60vh] sm:justify-end">
       <div className="h-[92vh] w-screen absolute top-0 left-0 -z-10">
         <Image
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           alt="poster"
-          layout='fill'
+          layout="fill"
           objectFit="cover"
         />
       </div>
+      <h1 className=" font-bold lg:text-7xl md:text-4xl text-4xl">
+        {movie?.name || movie?.title || movie?.original_name}
+      </h1>
+      <p className="max-w-xs md:max-w-lg  md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+      
     </div>
   );
 };
