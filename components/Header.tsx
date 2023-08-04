aa1a5ba9dae306adca6d8ccebcae67f18bf1c705
felixDev22 <felixnzomo@gmail.com>
2023-08-04 14:10:49 +0300

    Add Logo, notofication and user icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,10 @@
 import { SearchIcon } from '@heroicons/react/solid';
+import { BellIcon, UserIcon} from '@heroicons/react/solid';
 import Image from 'next/image';
+import Link from 'next/link';
+import {useState} from 'react';
+
+
 
 
 const Header = () => {
@@ -7,7 +12,7 @@ const Header = () => {
     <header>
       <div className="flex items-center justify-center space-x-2 md:space-x-10">
         <Image
-          src="/assets/movie+logo.jpg"
+          src="/assets/movie+logo.png"
           alt="logo"
           width={100}
           height={100}
@@ -20,11 +25,14 @@ const Header = () => {
           <li className="navLink">My List</li>
         </ul>
       </div>
-      <div>
-        <SearchIcon className="hidden sm:inline" />
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden sm:inline h-6 w-6" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="hidden sm:inline h-6 w-6" />
+        <UserIcon className="sm:inline h-6 w-6" />
       </div>
     </header>
   );
-}
+};
 
 export default Header
