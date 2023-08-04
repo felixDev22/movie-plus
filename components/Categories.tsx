2c0f4f48be8742c0e399e5f37d4c5ead201e599c
felixDev22 <felixnzomo@gmail.com>
2023-08-05 02:51:41 +0300

    Add scrolling arrows to the sides of the rows

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,14 +1,22 @@
-import { Movie } from "@/tsdefination"
+import { Movie } from '@/tsdefination';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
 
 interface Props {
-  title: string
-  movies: Movie[]
+  title: string;
+  movies: Movie[];
 }
 
-const Categories = ({title, movies}:Props) => {
+const Categories = ({ title, movies }: Props) => {
   return (
-    <div>Categories</div>
-  )
-}
+    <div className='h-40 space-y-1 md:space-y-3'>
+      <h2>{title}</h2>
+      <div className='row relative '>
+        <ChevronLeftIcon className=" h-8 w-8 cursor-pointer absolute top-0 bottom-0 left-5 z-30 m-auto hover:scale-100 opacity-0 transition row-hover:opacity-100" />
 
-export default Categories
+        <ChevronRightIcon className=" h-8 w-8 cursor-pointer absolute top-0 bottom-0 right-5 z-30 m-auto hover:scale-100 opacity-0 transition row-hover:opacity-100" />
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
