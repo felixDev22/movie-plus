3e8f09dc5a4a37e8dd1a0e37ad610f8c06bf6f40
felixDev22 <felixnzomo@gmail.com>
2023-08-05 02:50:39 +0300

    Update Hero, Add padding on hero text sm

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@ function Hero({ netflixOriginals }: Props ){
 
 
   return (
-    <div className="flex flex-col space-y-3 pt-40 lg:pb-12 lg:h-[70vh] lg:justify-end sm:h-[60vh] sm:justify-end">
+    <div>
       <div className="h-[92vh] w-screen absolute top-0 left-0 -z-10">
         <Image
           src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
@@ -28,18 +28,20 @@ function Hero({ netflixOriginals }: Props ){
           objectFit="cover"
         />
       </div>
-      <h1 className=" font-bold lg:text-7xl md:text-4xl text-4xl">
-        {movie?.name || movie?.title || movie?.original_name}
-      </h1>
-      <p className="max-w-xs md:max-w-lg  md:text-lg lg:max-w-2xl lg:text-2xl">
-        {movie?.overview}
-      </p>
+      <div className="flex flex-col space-y-3 pt-40 lg:pb-12 lg:h-[70vh] lg:justify-end sm:h-[60vh] sm:justify-end pb-16">
+        <h1 className=" font-bold lg:text-7xl md:text-4xl text-4xl">
+          {movie?.name || movie?.title || movie?.original_name}
+        </h1>
+        <p className="max-w-xs md:max-w-lg  md:text-lg lg:max-w-2xl lg:text-2xl">
+          {movie?.overview}
+        </p>
 
-      <div className="flex space-x-3">
-        <button className="btn  bg-[#db020d] text-white">
-          <PlayIcon className="h-5 w-5 text-black" /> Play
-        </button>
-        <button className="btn  bg-white text-black">More Info</button>
+        <div className="flex space-x-3">
+          <button className="btn  bg-[#db020d] text-white">
+            <PlayIcon className="h-5 w-5 text-black" /> Play
+          </button>
+          <button className="btn  bg-white text-black">More Info</button>
+        </div>
       </div>
     </div>
   );
