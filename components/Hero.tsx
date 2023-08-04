c992ac4c994757623def02676da5b2e36a64fa83
felixDev22 <felixnzomo@gmail.com>
2023-08-05 00:05:05 +0300

    Display poster, add objectFit to fix stretch. -z

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,11 +18,12 @@ function Hero({ netflixOriginals }: Props ){
 
   return (
     <div>
-      <div>
+      <div className="h-[92vh] w-screen absolute top-0 left-0 -z-10">
         <Image
-          src={`${baseUrl} ${movie?.backdrop_path || movie?.poster_path}`}
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
           alt="poster"
           layout='fill'
+          objectFit="cover"
         />
       </div>
     </div>
