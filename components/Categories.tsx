bac51f53040d6203d4ecb439fa7bd0d0d6a9be07
felixDev22 <felixnzomo@gmail.com>
2023-08-05 04:28:23 +0300

    Add dependencies, hide scroll bar

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -16,7 +16,7 @@ const Categories = ({ title, movies }: Props) => {
       <div className="group relative md:-ml-4 ">
         <ChevronLeftIcon className=" h-8 w-8 cursor-pointer absolute top-0 bottom-0 left-5 z-30 m-auto hover:scale-100 opacity-0 transition group-hover:opacity-100" />
 
-        <div className="flex overflow-x-scroll items-center space-x-1 md:space-x-3 md:p-2">
+        <div className="flex overflow-x-scroll scrollbar-hide   items-center space-x-1 md:space-x-3 md:p-2">
           {movies.map((movie) => (
             <Movieslist key={movie.id} movie={movie} />
           ))}
