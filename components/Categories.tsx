84dde4abddaf6a8e1ef68da7d275285f7562cb14
felixDev22 <felixnzomo@gmail.com>
2023-08-05 04:21:22 +0300

    Populate the movie thumbnails

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,5 +1,6 @@
 import { Movie } from '@/tsdefination';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import { Movieslist } from './Movieslist';
 
 interface Props {
   title: string;
@@ -8,10 +9,18 @@ interface Props {
 
 const Categories = ({ title, movies }: Props) => {
   return (
-    <div className='h-40 space-y-1 md:space-y-3'>
-      <h2>{title}</h2>
-      <div className='row relative '>
-        <ChevronLeftIcon className=" h-8 w-8 cursor-pointer absolute top-0 bottom-0 left-5 z-30 m-auto hover:scale-100 opacity-0 transition row-hover:opacity-100" />
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2 className="md:text-2xl font-semibold cursor-pointer hover:opacity-25 transition">
+        {title}
+      </h2>
+      <div className="group relative md:-ml-4 ">
+        <ChevronLeftIcon className=" h-8 w-8 cursor-pointer absolute top-0 bottom-0 left-5 z-30 m-auto hover:scale-100 opacity-0 transition group-hover:opacity-100" />
+
+        <div className="flex overflow-x-scroll items-center space-x-1 md:space-x-3 md:p-2">
+          {movies.map((movie) => (
+            <Movieslist key={movie.id} movie={movie} />
+          ))}
+        </div>
 
         <ChevronRightIcon className=" h-8 w-8 cursor-pointer absolute top-0 bottom-0 right-5 z-30 m-auto hover:scale-100 opacity-0 transition row-hover:opacity-100" />
       </div>
